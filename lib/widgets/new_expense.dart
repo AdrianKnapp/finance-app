abc605ddbcb05158bd0119565e734171d4733ae7
AdrianKnapp <adrknapp@gmail.com>
2025-04-02 23:46:34 -0300

    feat(expenses): adding expenses

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -5,7 +5,9 @@ import 'package:intl/intl.dart';
 final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -63,7 +65,14 @@ class _NewExpenseState extends State<NewExpense> {
       return;
     }
 
-    // ...
+    widget.onAddExpense(Expense(
+      amount: enteredAmount,
+      title: _titleController.text,
+      date: _selectedDate!,
+      category: _selectedCategory,
+    ));
+
+    Navigator.pop(context);
   }
 
   @override
